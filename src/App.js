222c8db5eb957f767e44ee72897da512df1a0f11
iPanda <aungkyawkhaing2000@gmail.com>
2022-08-25 00:05:26 +0630

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import About from './Router/About'
+import FeatureProduct from './Router/FeatureProduct'
+import Home from './Router/Home'
+import Navbar from './Router/Navbar'
+import OrderSummary from './Router/OrderSummary'
+import Product from './Router/Product'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Navbar/>
+        <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='about' element={<About/>}></Route>
+            <Route path='order-summary' element={<OrderSummary/>}></Route>
+            <Route path='product' element={<Product/>} >
+                 <Route path='featured' element={<FeatureProduct/>}></Route>
+            </Route>
+            <Route></Route>
+        </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
